35c8b4a1941fa5b516b06216cb06259a12929c96
Markus Zapke-Gründemann <markuszapke@gmx.net>
2010-05-20 16:35:14 +0200

    Code der ersten Applikation mit fertigen Models.

---
+++ src/cookbook/recipes/models.py
@@ -0,0 +1,52 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(u'Name', max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(u'Beschreibung', blank=True)
+
+    class Meta:
+        pass
+
+    def __unicode__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    DIFFICULTY_EASY = 1
+    DIFFICULTY_MEDIUM = 2
+    DIFFICULTY_HARD = 3
+    DIFFICULTIES = (
+        (DIFFICULTY_EASY, u'einfach'),
+        (DIFFICULTY_MEDIUM, u'normal'),
+        (DIFFICULTY_HARD, u'schwer'),
+    )
+    title = models.CharField(u'Titel', max_length=255)
+    slug = models.SlugField(unique=True)
+    ingredients = models.TextField(u'Zutaten',
+        help_text=u'Eine Zutat pro Zeile angeben')
+    preparation = models.TextField(u'Zubereitung')
+    time_for_preparation = models.IntegerField(u'Zubereitungszeit',
+        blank=True, null=True)
+    number_of_portions = models.IntegerField(u'Anzahl der Portionen')
+    difficulty = models.SmallIntegerField(u'Schwierigkeitsgrad',
+        choices=DIFFICULTIES, default=DIFFICULTY_MEDIUM)
+    category = models.ManyToManyField(Category, verbose_name=u'Kategorie')
+    author = models.ForeignKey(User, verbose_name=u'Autor')
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(editable=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __unicode__(self):
+        return self.title
+    
+    def save(self, force_insert=False, force_update=False):
+        if not self.id:
+            self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        super(Recipe, self).save(force_insert, force_update)
