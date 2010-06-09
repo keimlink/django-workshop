249626b39ed2d68d1c5721e63fe8f15d8cdf08aa
Markus Zapke-Gründemann <markuszapke@gmx.net>
2010-06-09 22:43:52 +0200

    URLConf für Rezept-Formulare erweitert.

--- src/cookbook/recipes/urls.py
+++ src/cookbook/recipes/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('recipes.views',
     url(r'^rezept/(?P<slug>[-\w]+)/$', 'detail', name='recipes_recipe_detail'),
+    url(r'^erstellen/$', 'add', name='recipes_recipe_add'),
+    url(r'^bearbeiten/(?P<recipe_id>\d+)/$', 'edit', name='recipes_recipe_edit'),
     url(r'^$', 'index', name='recipes_recipe_index'),
 )
