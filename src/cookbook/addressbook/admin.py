e3132997a0c5d327315355ef880906497e96f2d1
Markus Zapke-Gründemann <info@keimlink.de>
2011-11-11 00:10:41 +0100

    Admin verbessert und erweitert.

--- src/cookbook/addressbook/admin.py
+++ src/cookbook/addressbook/admin.py
@@ -7,8 +7,8 @@ class AddressAdmin(admin.ModelAdmin):
     actions = None
     list_display = ('first_name', 'last_name', 'street', 'zipcode', 'city')
     list_display_links = ('first_name', 'last_name')
-    list_filter = ('city',)
-    search_fields = ['first_name', 'last_name', 'street', 'zipcode', 'city']
+    list_filter = ('city', 'last_name')
+    search_fields = ['first_name', 'last_name', 'street', 'zipcode', 'city__name']
     readonly_fields = ('first_name', 'last_name', 'street', 'zipcode', 'city')
 
 
