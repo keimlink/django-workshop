9bb5c9961e147fd16d2eaef0631980c50191d776
Markus Zapke-Gründemann <markus@keimlink.de>
2012-07-02 10:24:26 +0200

    Moved to Task classes and added BuildPdfTask.

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,62 @@
+import os
+import webbrowser
+
 from fabric.api import *
 from fabric.contrib.console import confirm
+from fabric.tasks import Task
+
+
+class DjangoWorkshopBaseTask(Task):
+    def __init__(self, *args, **kwargs):
+        super(DjangoWorkshopBaseTask, self).__init__(*args, **kwargs)
+        self.prj_root = os.path.realpath(os.path.dirname(__file__))
+        self.docs = os.path.join(self.prj_root, 'docs')
+
+
+class BuildHtmlTask(DjangoWorkshopBaseTask):
+    """Builds the Sphinx documentation as HTML."""
+    name = 'build'
+
+    def run(self, open_browser=True):
+        with lcd(self.docs):
+            local('make clean')
+            local('make html')
+        if open_browser:
+            webbrowser.open(os.path.join(self.docs, '_build/html/index.html'))
+
+
+class BuildPdfTask(DjangoWorkshopBaseTask):
+    """Builds the Sphinx documentation as PDF."""
+    name = 'build_pdf'
+
+    def run(self):
+        with lcd(self.docs):
+            local('make clean')
+            local('make latexpdf')
+        pdfpath = os.path.join(self.docs, '_build/latex/DjangoWorkshop.pdf')
+        try:
+            os.startfile(pdfpath)
+        except AttributeError:
+            local('open %s' % pdfpath)
+
+
+class DeployTask(BuildHtmlTask):
+    """Builds and deploys the Sphinx documentation."""
+    name = 'deploy'
+
+    def run(self):
+        super(DeployTask, self).run(open_browser=False)
+        if not confirm('Do you wish to deploy this build?'):
+            abort('Deployment cancelled.')
+        with cd('doms/django-workshop.de/subs'):
+            run('rm -rf www')
+            run('mkdir www')
+            put('docs/_build/html/*', 'www')
+            put('docs/.htaccess', 'www')
 
 
 env.user = 'zed00-keimlink'
 env.hosts = ['zed00.hostsharing.net']
-
-
-@task
-def build():
-    """Builds the Sphinx documentation."""
-    with lcd('docs'):
-        local('make clean')
-        local('make html')
-
-
-@task
-def deploy():
-    """Builds and deploys the Sphinx documentation."""
-    build()
-    if not confirm('Do you wish to deploy this build?'):
-        abort('Deployment cancelled.')
-    with cd('doms/django-workshop.de/subs'):
-        run('rm -rf www')
-        run('mkdir www')
-        put('docs/_build/html/*', 'www')
-        put('docs/.htaccess', 'www')
+build = BuildHtmlTask()
+build_pdf = BuildPdfTask()
+deploy = DeployTask()
