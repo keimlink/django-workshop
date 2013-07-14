6ba86c12b8dc3717620023eec4e9543f1a2df747
Markus Zapke-Gründemann <markus@keimlink.de>
2013-07-14 05:57:48 +0200

    Address.__unicode__() was not returning an unicode object.

--- src/cookbook/addressbook/models.py
+++ src/cookbook/addressbook/models.py
@@ -14,7 +14,7 @@ class Address(models.Model):
         managed = False
 
     def __unicode__(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        return u'%s %s' % (self.first_name, self.last_name)
 
 
 class City(models.Model):
